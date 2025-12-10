88ac9f4a058db50e66f498ae91e1a0db5c7b2813
David <davidferrersapinya@gmail.com>
2025-12-10 12:10:27 +0100

    Initial commit

---
+++ src/main/java/com/david/facturacion_producto/FacturacionProductoApplication.java
@@ -0,0 +1,13 @@
+package com.david.facturacion_producto;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FacturacionProductoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FacturacionProductoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/david/facturacion_producto/FacturacionProductoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.david.facturacion_producto;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FacturacionProductoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
